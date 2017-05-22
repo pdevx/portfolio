6c75e3ac656024e0baa50054f333ea8ddd9ed36c
James Hester <pdevx@Apples-MacBook-Pro.local>
2017-05-22 00:46:26 -0700

    Trying things out in Heroku.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hey');
+  res.send('Hey you guys');
   // res.sendFile('app/.www/index.html');
 })
 
