678d9ab5eaa31d28264c42ea2d60f4ce72bf14ba
James Hester <pdevx@Apples-MacBook-Pro.local>
2017-05-31 16:12:59 -0700

    Initial commit for Cloud Connect Project view.

--- app/components/projects/cloud-connect/projects.cloud-connect.controller.js
+++ app/components/projects/cloud-connect/projects.cloud-connect.controller.js
@@ -13,11 +13,51 @@ projectsCloudConnectController.controller('projectsCloudConnectController', func
         img: "assets/img/cloud-connect_square.png",
         description: " Served as principal developer for web application built with Angular and Angular Material. Helped develop serverless architecture for web application using the AWS Javascript SDK, AWS DynamoDB, AWS API Gateway, AWS IoT, and AWS Lambdas.",
         features: [
-            // {
-            //     title: "",
-            //     image: "",
-            //     description: ""
-            // }
+            {
+                title: "Printer From Dashboard",
+                img: "assets/img/cloudconnect_dashboard.png",
+                description: ""
+            },
+            {
+                title: "Device Management",
+                img: "assets/img/cloudconnect_devices.png",
+                description: ""
+            },
+            {
+                title: "Print Without Drivers",
+                img: "assets/img/cloudconnect_print.png",
+                description: ""
+            },
+            {
+                title: "Scan Without Drivers",
+                img: "assets/img/cloudconnect_scan.png",
+                description: ""
+            },
+            {
+                title: "Cognito User Authentication",
+                img: "assets/img/cloudconnect_login.gif",
+                description: ""
+            },
+            {
+                title: "Angular + Material Design",
+                img: "assets/img/cloudconnect_material.gif",
+                description: ""
+            },
+            {
+                title: "User Management",
+                img: "assets/img/cloudconnect_users.gif",
+                description: ""
+            },
+            {
+                title: "DynamoDB For Data",
+                img: "assets/img/cloudconnect_user_profile.gif",
+                description: ""
+            },
+            {
+                title: "Device Registration",
+                img: "assets/img/cloudconnect_device_register.gif",
+                description: ""
+            }
         ]
     };
 
